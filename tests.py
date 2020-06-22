f1b808a26d53915c0cd51fb3d3d93a00d3138027
Ben Fulton <befulton@iu.edu>
2020-06-22 16:00:41 -0400

    Use CUDA_AVAILABLE_DEVICES to determine which devices to process

--- tests.py
+++ tests.py
@@ -1,13 +1,28 @@
 import unittest
-import gpu_accounting
-import ConfigParser
+import json
+import sys
+
+## rename record_gpu_usage to record_gpu_usage.py to run tests
+import record_gpu_usage
+from unittest.mock import patch
+import os
+
+if sys.version_info[0] < 3:
+    import ConfigParser
+else:
+    import configparser as ConfigParser
 
 class TestStringMethods(unittest.TestCase):
 
     def test_to_json(self):
         rows = [["a", "b", "c", "d", "e", "f"]]
-        expected = """[{"pid": "b", "time": "f", "gpuUtilization": "c", "serial": "a", "maxMemoryUsage": "e", "memoryUtilization": "d"}]"""
-        self.assertEqual(gpu_accounting.to_json(rows), expected)
+        actual = json.loads(record_gpu_usage.to_json(rows))
+        self.assertEqual('a', actual[0]['serial'])
+        self.assertEqual('b', actual[0]['pid'])
+        self.assertEqual('c', actual[0]['gpuUtilization'])
+        self.assertEqual('d', actual[0]['memoryUtilization'])
+        self.assertEqual('e', actual[0]['maxMemoryUsage'])
+        self.assertEqual('f', actual[0]['time'])
 
     def test_get_config(self):
         config = ConfigParser.ConfigParser()
@@ -16,7 +31,7 @@ class TestStringMethods(unittest.TestCase):
         config.set('CLUSTER', 'name', 'test_cluster')
         config.set('DATABASE', 'user', 'bob')
         config.set('DATABASE', 'password', 'secret')
-        cluster, db = gpu_accounting.get_config(config)
+        cluster, db = record_gpu_usage.get_config(config)
         self.assertEqual(cluster, 'test_cluster')
         self.assertEqual(db, {'user':'bob', 'password':'secret'})
 
@@ -26,6 +41,18 @@ class TestStringMethods(unittest.TestCase):
         # check that s.split fails when the separator is not a string
         with self.assertRaises(TypeError):
             s.split(2)
+
+    @patch("record_gpu_usage.nvmlDeviceGetHandleByIndex")
+    @patch("record_gpu_usage.nvmlDeviceClearAccountingPids")
+    def test_ClearAccounting(self, b, gethandle):
+        os.environ['CUDA_VISIBLE_DEVICES'] = "1,3,5"
+        record_gpu_usage.ClearAccounting()
+        actual = [x[0][0] for x in gethandle.call_args_list]
+        self.assertEquals(actual, [1, 3, 5])
+
+    def test_available_devices(self):
+        os.environ['CUDA_VISIBLE_DEVICES'] = "1,3,5"
+        self.assertEqual([1, 3, 5], list(record_gpu_usage.available_devices()))
 
 if __name__ == "__main__":
     unittest.main()
